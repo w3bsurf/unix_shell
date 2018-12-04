606c037485ae1c854038bebfa4a6d563610bf5d5
Joonas Hakkarainen <joonas.hakkarainen@elisanet.fi>
2018-12-04 19:24:33 +0200

    Changed execvp to execv

--- main.c
+++ main.c
@@ -28,74 +28,98 @@ int main(int argc, char **argv) {
 	
 	signal(SIGALRM, signal_handler);
 	signal(SIGINT, signal_handler);
-	
-	if (argc == 1) {
+
+	char path[MAXLEN] = "/bin/";
+
+	line = (char *)malloc(buffer_size * sizeof(char));
+    if( line == NULL) {
+        perror("Unable to allocate buffer");
+        exit(1);
+    }
+
+	if (argc == 1) { /* Run in interactive mode if invoked with no arguments */
 		while (1) {
-		background = 0;
-		
-		/* print the prompt */
-		printf("wish> ");
-		/* set the timeout for alarm signal (autologout) */
-		alarm(LOGOUT);
-		
-		/* read the users command */
-		getline(&line, &buffer_size, stdin); 
-		if (line == NULL) {
-			printf("\nlogout\n");
-			exit(0);
-		}
-		line[strlen(line) - 1] = '\0';
-		
-		if (strlen(line) == 0) {
-			continue;
-		}
-		
-		/* start to background? */
-		if (line[strlen(line)-1] == '&') {
-			line[strlen(line)-1]=0;
-			background = 1;
-		}
-		
-		/* split the command line */
-		i = 0;
-		cmd = line;
-		while ((args[i] = strtok(cmd, " ")) != NULL) {
-			printf("arg %d: %s\n", i, args[i]);
-			i++;
-			cmd = NULL;
-		}
-		
-		if (strcmp(args[0],"exit")==0) {
-			exit(0);
-		}
-		
-		/* fork to run the command */
-		switch (pid = fork()) {
-			case -1:
-				/* error */
-				perror("fork");
+			background = 0;
+			char * new_str;
+
+						
+			/* Print the prompt */
+			printf("wish> ");
+			/* Set the timeout for alarm signal (autologout) */
+			alarm(LOGOUT);
+			
+			/* Read the users command */
+			getline(&line, &buffer_size, stdin); 
+			if (line == NULL) {
+				printf("\nlogout\n");
+				exit(0);
+			}
+			line[strlen(line) - 1] = '\0';
+			
+			if (strlen(line) == 0) {
 				continue;
-			case 0:
-				/* child process */
-				execvp(args[0], args);
-				perror("execvp");
-				exit(1);
-			default:
-				/* parent (shell) */
-				if (!background) {
-					alarm(0);
-					//waitpid(pid, NULL, 0);
-					while (wait(NULL)!=pid)
-						printf("Some other child process exited\n");
-				}
-				break;
+			}
+			
+			/* Start to background? */
+			if (line[strlen(line)-1] == '&') {
+				line[strlen(line)-1]=0;
+				background = 1;
+			}
+			
+			/* Split the command line */
+			i = 0;
+			cmd = line;
+			while ((args[i] = strtok(cmd, " ")) != NULL) {
+				printf("arg %d: %s\n", i, args[i]);
+				i++;
+				cmd = NULL;
+			}
+			
+			if (strcmp(args[0],"exit")==0) {
+				free(line);
+				free(new_str);
+				exit(0);
+			}
+
+			
+			if((new_str = malloc(strlen(path)+strlen(args[0])+1)) != NULL){
+			    new_str[0] = '\0';
+			    strcat(new_str,path);
+			    strcat(new_str,args[0]);
+			} else {
+			   	perror("Malloc");
+			    exit(1);
+			}
+			
+			/* Fork to run the command */
+			switch (pid = fork()) {
+				case -1:
+					/* error */
+					perror("fork");
+					continue;
+				case 0:
+					/* child process */
+					execv(new_str, args);
+					perror("execv");
+					free(line);
+					free(new_str);
+					exit(1);
+				default:
+					/* parent (shell) */
+					if (!background) {
+						alarm(0);
+						//waitpid(pid, NULL, 0);
+						while (wait(NULL)!=pid)
+							printf("Some other child process exited\n");
+					}
+					break;
+			}
 		}
-	}
 		
-	} else if (argc == 2) {
+	} else if (argc == 2) { /* Run in batch mode if invoked with one argument */
 		printf("Batch mode\n");
-	} else {  /* Terminate program if shell invoked with more than 1 argument */
-		perror("Shell can only be invoked with no arguments or a single argument\n");
+	} else {  /* Terminate program if shell invoked with more than one argument */
+		printf("Shell can only be invoked with no arguments or a single argument\n");
 		exit(1);
 	}
 	
